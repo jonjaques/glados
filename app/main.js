e66451363f67dfd7f7a240d5800aa71c71a8b625
Jon Jaques <jon@jonjaques.com>
2013-05-06 15:36:42 -0500

    Convert to tabs

--- app/main.js
+++ app/main.js
@@ -1,28 +1,28 @@
 ﻿requirejs.config({
-    paths: {
-        'text': 'core/amd/text'
-    }
+		paths: {
+				'text': 'core/amd/text'
+		}
 });
 
 define(['core/app', 'core/system', 'core/viewLocator'],
-  function (app, system, viewLocator) {
+	function (app, system, viewLocator) {
 
-    system.debug(true);
+		system.debug(true);
 
 		var MyApp = {
 			app: app,
 			system: system
 		};
 
-    window.MyApp = window.MyApp || MyApp;
+		window.MyApp = window.MyApp || MyApp;
 
-    app.title = 'Durandal Samples';
-    app.start().then(function () {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
-        viewLocator.useConvention();
+		app.title = 'Durandal Samples';
+		app.start().then(function () {
+				//Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+				//Look for partial views in a 'views' folder in the root.
+				viewLocator.useConvention();
 
-        app.adaptToDevice();
-        app.setRoot('../samples/shell');
-    });
+				app.adaptToDevice();
+				app.setRoot('../samples/shell');
+		});
 });
