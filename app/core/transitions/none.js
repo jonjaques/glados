78492a55a4bcf596300b420f5b3f7d09c0a5a665
Jon Jaques <jon@jonjaques.com>
2013-05-06 13:41:11 -0500

    Add some transitions.

---
+++ app/core/transitions/none.js
@@ -0,0 +1,64 @@
+define(['../system'], function(system) {
+	var fadeOutDuration = 100;
+
+	var entrance = function(parent, newChild, settings) {
+		return system.defer(function(dfd) {
+			function endTransition() {
+				dfd.resolve();
+			}
+
+			function scrollIfNeeded() {
+				if (!settings.keepScrollPosition) {
+					$(document).scrollTop(0);
+				}
+			}
+
+			if (!newChild) {
+				scrollIfNeeded();
+
+				if (settings.activeView) {
+					$(settings.activeView).fadeOut(fadeOutDuration, function () {
+						if (!settings.cacheViews) {
+							ko.virtualElements.emptyNode(parent);
+						}
+						endTransition();
+					});
+				} else {
+					if (!settings.cacheViews) {
+						ko.virtualElements.emptyNode(parent);
+					}
+					endTransition();
+				}
+			} else {
+				var $previousView = $(settings.activeView);
+				var duration = settings.duration || 500;
+				var fadeOnly = !!settings.fadeOnly;
+
+				function startTransition() {
+					scrollIfNeeded();
+
+					if (settings.cacheViews) {
+						if (settings.composingNewView) {
+							ko.virtualElements.prepend(parent, newChild);
+						}
+					} else {
+						ko.virtualElements.emptyNode(parent);
+						ko.virtualElements.prepend(parent, newChild);
+					}
+
+					$(newChild).show();
+					endTransition();
+				}
+
+				if ($previousView.length) {
+					$previousView.hide();
+					startTransition();
+				} else {
+					startTransition();
+				}
+			}
+		}).promise();
+	};
+
+	return entrance;
+});
